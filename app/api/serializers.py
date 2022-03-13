34560cd1907aac3d1304be8de76626437ba7ad34
Nitesh Yadav <niteshyadav071@gmail.com>
2022-03-13 19:13:41 +0530

    updates

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -1,8 +1,22 @@
 from django.utils.translation import activate
 from rest_framework import serializers
+from app.models import Movie
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
     description = serializers.CharField()
     active = serializers.BooleanField()
+
+# create method for post requests 
+    def create(self,validated_data):
+        #this return gives the data to the views.py in post request method
+        return Movie.objects.create(**validated_data)
+
+# instanse carry old data and validated data carry new data
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.discription = validated_data.get('description', instance.name)
+        instance.active = validated_data.get('active',instance.active)
+        instance.save()
+        return instance
--- app/views.py
+++ app/views.py
@@ -1,23 +1,23 @@
-# from django.shortcuts import render
-# from app.models import Movie
-# from django.http import JsonResponse
+from django.shortcuts import render
+from app.models import Movie
+from django.http import JsonResponse
 
-# # Create your views here.
-# # def home(request):
-# #     return render(request,'home.html')
-# def movie_list(request):
-#     movies  = Movie.objects.all()
-#     data = {
-#         'movies' : list(movies.values())
-#     }
-#     return JsonResponse(data)
+# Create your views here.
+# def home(request):
+#     return render(request,'home.html')
+def movie_list(request):
+    movies  = Movie.objects.all()
+    data = {
+        'movies' : list(movies.values())
+    }
+    return JsonResponse(data)
 
-# def movie_details(request, pk):
-#     movie = Movie.objects.get(pk=pk)
-#     data = {
-#         'name' : movie.name,
-#         'description' : movie.description,
-#         'active' : movie.active
-#     }
-#     #movie = Movie.objects.get(pk=pk)
-#     return JsonResponse(data) 
+def movie_details(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    data = {
+        'name' : movie.name,
+        'description' : movie.description,
+        'active' : movie.active
+    }
+    #movie = Movie.objects.get(pk=pk)
+    return JsonResponse(data) 
